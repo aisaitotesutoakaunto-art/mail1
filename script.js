22f33749ba8fed528ea8be11754642b4e3f737ce
aisaitotesutoakaunto-art <aisaitotesutoakaunto@gmail.com>
2026-01-29 17:24:47 +0900

    Create script.js

---
+++ script.js
@@ -0,0 +1,27 @@
+const API_URL = "https://script.google.com/macros/s/AKfycbwUQ2SVfCHllTh5z7SuAoRHPBfD9CBQjhuuxhTE6m_wRZ3Py_74uzD4I43iJU4KoPK26A/exec";
+
+document.getElementById("send").addEventListener("click", async () => {
+  // フォームの値を取得
+  const data = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value
+  };
+
+  try {
+    const res = await fetch(API_URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    });
+
+    const result = await res.json();
+    alert(result.message);
+
+  } catch (error) {
+    alert("送信に失敗しました");
+    console.error(error);
+  }
+});
