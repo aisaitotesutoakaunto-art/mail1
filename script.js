51b50c624fca5f5f50634c59c0f4dabb272e921e
aisaitotesutoakaunto-art <aisaitotesutoakaunto@gmail.com>
2026-01-29 17:50:30 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 const API_URL = "https://script.google.com/macros/s/AKfycbwP_vUhLK-WcsQBQvYAebrX13eCwFiw3wqxMERmXf2p0vsvNgQkbpc32w5mFVQL6knm/exec";
 
 document.getElementById("send").addEventListener("click", async () => {
-  // フォームの値を取得
   const data = {
     name: document.getElementById("name").value,
     email: document.getElementById("email").value,
@@ -12,16 +11,16 @@ document.getElementById("send").addEventListener("click", async () => {
     const res = await fetch(API_URL, {
       method: "POST",
       headers: {
-        "Content-Type": "application/json"
+        "Content-Type": "text/plain;charset=utf-8"
       },
       body: JSON.stringify(data)
     });
 
-    const result = await res.json();
-    alert(result.message);
+    const text = await res.text(); // ← json() じゃない
+    alert("送信成功");
 
-  } catch (error) {
-    alert("送信に失敗しました");
-    console.error(error);
+  } catch (e) {
+    console.error(e);
+    alert("送信失敗");
   }
 });
